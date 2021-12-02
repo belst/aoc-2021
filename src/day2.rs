7a3da1dd24ce216b7d19e1bcfed2831affb45b6e
belst <git@bel.st>
2021-12-02 11:36:05 +0100

    rustfmt

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,3 @@
-
 pub enum Command {
     Forward(usize),
     Down(usize),
@@ -6,8 +5,9 @@ pub enum Command {
 }
 
 pub fn generate(input: &str) -> Vec<Command> {
-    input.lines()
-        .filter_map(|l| l.rsplit_once(" "))
+    input
+        .lines()
+        .filter_map(|l| l.split_once(" "))
         .map(|t| match t {
             ("forward", n) => Command::Forward(n.parse().unwrap()),
             ("down", n) => Command::Down(n.parse().unwrap()),
@@ -27,11 +27,13 @@ pub fn part1(input: &[Command]) -> usize {
 }
 
 pub fn part2(input: &[Command]) -> usize {
-    let (h, d, _a) = input.iter().fold((0usize, 0, 0usize), |(h, d, a), c| match c {
-        Command::Forward(n) => (h + n, d + n * a, a),
-        Command::Down(n) => (h, d, a + n),
-        Command::Up(n) => (h, d, a - n),
-    });
+    let (h, d, _a) = input
+        .iter()
+        .fold((0usize, 0, 0usize), |(h, d, a), c| match c {
+            Command::Forward(n) => (h + n, d + n * a, a),
+            Command::Down(n) => (h, d, a + n),
+            Command::Up(n) => (h, d, a - n),
+        });
     h * d
 }
 
@@ -57,7 +59,4 @@ forward 2
         let input = super::generate(TESTINPUT);
         assert_eq!(super::part2(&input), 900);
     }
-    
 }
-
-
